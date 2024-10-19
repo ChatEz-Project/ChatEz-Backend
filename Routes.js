9307337b7238079de0339b4e27870094af764da4
Abdul Rehman <87334764+X2Abdul@users.noreply.github.com>
2024-10-19 18:01:38 +0100

    added cors to allow frontend to hit backend

--- Routes.js
+++ Routes.js
@@ -2,15 +2,25 @@ const express = require("express");
 const userController = require("./src/user/Controller");
 const routes = express();
 
+const cors = require("cors");
+routes.use(cors()); // Enable CORS for all routes
+routes.use(express.json()); // Middleware to parse JSON request bodies
+
 // Define routes
 routes.post("/addFriend/:email", userController.addFriend);
 
 // Only call listen() when running the app directly
 if (require.main === module) {
-    const PORT = 8080;
-    routes.listen(PORT, () => {
-        console.log(`Server started on port http://localhost:${PORT}`);
-    });
+  const PORT = 8080;
+  routes.listen(PORT, () => {
+    console.log(`Server started on port http://localhost:${PORT}`);
+  });
 }
+
+routes.use(
+  cors({
+    origin: "http://localhost:3000", // Allow only this origin
+  })
+);
 
 module.exports = routes; // Export the app (without starting the server)
