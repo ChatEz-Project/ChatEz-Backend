06e4cd4619280d7b3bb9b023462a8d22b479322c
Joel <91616550+Stegi56@users.noreply.github.com>
2024-11-18 17:46:11 +0000

    Add optimised getMessage endpoints and implicit message read setting

--- src/message/Controller.js
+++ src/message/Controller.js
@@ -54,7 +54,46 @@ const getMessages = async (req, res) => {
   }
 }
 
+const getMessagesForSidebar = async (req, res) => {
+  try{
+    const client = Auth.getClientEmail(req);
+    console.log(`Getting messages for ${client}`);
+    const messages = await MessageConnector.getLatestMessageFromEachConversation(client);
+    return res.status(200).send(messages);
+  }catch (err){
+    console.error(err);
+    return res.status(500).send(`Server Error: ${err}`);
+  }
+}
+
+const getMessagesForFriend = async (req, res) => {
+  try{
+    const clientEmail = Auth.getClientEmail(req);
+    const { friendEmail } = req.params;
+    await setFriendMessagesToRead(clientEmail, friendEmail);
+    console.log("set messages to read")
+    console.log(`Getting messages for ${clientEmail}`);
+    const messages = await MessageConnector.getFriendMessages(clientEmail, friendEmail);
+    return res.status(200).send(messages);
+  }catch (err){
+    console.error(err);
+    return res.status(500).send(`Server Error: ${err}`);
+  }
+}
+
+const setFriendMessagesToRead = async (clientEmail, friendEmail) => {
+  try{
+    console.log(`Getting setting messages from ${friendEmail} to read by ${clientEmail}`);
+    return await MessageConnector.setFriendMessageRead(clientEmail, friendEmail);
+  }catch (err){
+    console.error("Error setting messages to read");
+    err;
+  }
+}
+
 module.exports = {
   sendMessage,
-  getMessages
+  getMessages,
+  getMessagesForSidebar,
+  getMessagesForFriend
 }
