939f03a7e5f59f97395e0e93f200a127d6ba91d9
Abdul Rehman <87334764+X2Abdul@users.noreply.github.com>
2024-12-28 23:52:31 +0000

    delete conversation

--- src/message/Controller.js
+++ src/message/Controller.js
@@ -1,99 +1,126 @@
 const MessageConnector = require("./MongoConnector");
 const FirebaseStorageConnector = require("./FirebaseStorageConnector");
 const UserConnector = require("../user/MongoConnector");
-const Message = require("./Model")
+const Message = require("./Model");
 const Auth = require("../Auth");
 
-const dotenv = require('dotenv');
-const environment = process.env.NODE_ENV || 'dev';
+const dotenv = require("dotenv");
+const environment = process.env.NODE_ENV || "dev";
 dotenv.config({ path: `.env.${environment}` });
 
 const sendMessage = async (req, res) => {
-  try{
+  try {
     const sender = Auth.getClientEmail(req);
     const { recipient } = req.params;
     const messageContent = req.body.message;
 
     if (!messageContent) {
-      return res.status(400).send('Message content required in body');
+      return res.status(400).send("Message content required in body");
     }
 
-    if(await UserConnector.getUser(recipient) == null){
+    if ((await UserConnector.getUser(recipient)) == null) {
       return res.status(404).send(`recipient ${recipient} not found`);
     }
 
     let fileUrl = null;
-    if(req.file != null){
-      fileUrl = await FirebaseStorageConnector.uploadFileToFirebase(req.file, sender);
+    if (req.file != null) {
+      fileUrl = await FirebaseStorageConnector.uploadFileToFirebase(
+        req.file,
+        sender
+      );
       console.log(`uploaded message file: ${fileUrl}`);
     }
 
     const message = new Message({
-      sender   : sender,
+      sender: sender,
       recipient: recipient,
-      fileUrl  : fileUrl,
-      message  : messageContent
-    })
+      fileUrl: fileUrl,
+      message: messageContent,
+    });
 
-    await MessageConnector.storeNewMessage(message)
+    await MessageConnector.storeNewMessage(message);
     return res.status(200).send("Message sent successfully");
-  }catch(err){
+  } catch (err) {
     return res.status(500).send(`Server Error: ${err}`);
   }
-}
+};
 
 const getMessages = async (req, res) => {
-  try{
+  try {
     const client = Auth.getClientEmail(req);
     console.log(`Getting messages for ${client}`);
     const messages = await MessageConnector.getUserMessages(client);
     return res.status(200).send(messages);
-  }catch (err){
+  } catch (err) {
     console.error(err);
     return res.status(500).send(`Server Error: ${err}`);
   }
-}
+};
 
 const getMessagesForSidebar = async (req, res) => {
-  try{
+  try {
     const client = Auth.getClientEmail(req);
     console.log(`Getting messages for ${client}`);
-    const messages = await MessageConnector.getLatestMessageFromEachConversation(client);
+    const messages =
+      await MessageConnector.getLatestMessageFromEachConversation(client);
     return res.status(200).send(messages);
-  }catch (err){
+  } catch (err) {
     console.error(err);
     return res.status(500).send(`Server Error: ${err}`);
   }
-}
+};
 
 const getMessagesForFriend = async (req, res) => {
-  try{
+  try {
     const clientEmail = Auth.getClientEmail(req);
     const { friendEmail } = req.params;
     await setFriendMessagesToRead(clientEmail, friendEmail);
-    console.log("set messages to read")
+    console.log("set messages to read");
     console.log(`Getting messages for ${clientEmail}`);
-    const messages = await MessageConnector.getFriendMessages(clientEmail, friendEmail);
+    const messages = await MessageConnector.getFriendMessages(
+      clientEmail,
+      friendEmail
+    );
     return res.status(200).send(messages);
-  }catch (err){
+  } catch (err) {
     console.error(err);
     return res.status(500).send(`Server Error: ${err}`);
   }
-}
+};
+
+const deleteConversation = async (req, res) => {
+  try {
+    const clientEmail = Auth.getClientEmail(req);
+    const { friendEmail } = req.params;
+    await MessageConnector.deleteConversation(clientEmail, friendEmail);
+    return res
+      .status(200)
+      .send(`Conversation deleted for friend ${friendEmail}`);
+  } catch (err) {
+    console.error(err);
+    return res.status(500).send(`Server Error: ${err}`);
+  }
+};
 
 const setFriendMessagesToRead = async (clientEmail, friendEmail) => {
-  try{
-    console.log(`Getting setting messages from ${friendEmail} to read by ${clientEmail}`);
-    return await MessageConnector.setFriendMessageRead(clientEmail, friendEmail);
-  }catch (err){
+  try {
+    console.log(
+      `Getting setting messages from ${friendEmail} to read by ${clientEmail}`
+    );
+    return await MessageConnector.setFriendMessageRead(
+      clientEmail,
+      friendEmail
+    );
+  } catch (err) {
     console.error("Error setting messages to read");
     err;
   }
-}
+};
 
 module.exports = {
   sendMessage,
   getMessages,
   getMessagesForSidebar,
-  getMessagesForFriend
-}
+  getMessagesForFriend,
+  deleteConversation,
+};
--- src/message/MongoConnector.js
+++ src/message/MongoConnector.js
@@ -1,90 +1,84 @@
-const accessSecret = require('../SecretManager');
-const Message = require('./Model');
+const accessSecret = require("../SecretManager");
+const Message = require("./Model");
 
 const dotenv = require("dotenv");
-const mongoose = require('mongoose')
+const mongoose = require("mongoose");
 
-const environment = process.env.NODE_ENV || 'dev';
+const environment = process.env.NODE_ENV || "dev";
 dotenv.config({ path: `.env.${environment}` });
 
 async function connectToDatabase() {
   try {
-    const mongoPassword = await accessSecret(`Mongo_${process.env.MONGO_USERNAME}`);
+    const mongoPassword = await accessSecret(
+      `Mongo_${process.env.MONGO_USERNAME}`
+    );
     const dbURI = `mongodb+srv://${process.env.MONGO_USERNAME}:${mongoPassword}@chatez.9mxcu.mongodb.net/${process.env.MONGO_DB}?retryWrites=true&w=majority&appName=ChatEz`;
     await mongoose.connect(dbURI, {
-      maxPoolSize: process.env.MONGO_MAX_CONNECTIONS
+      maxPoolSize: process.env.MONGO_MAX_CONNECTIONS,
     });
   } catch (err) {
-    console.error('Database connection error:', err); throw err;
+    console.error("Database connection error:", err);
+    throw err;
   }
 }
 
-async function storeNewMessage(message){
-  try{
-    return await message.save()
-  }catch(err){
-    console.error(err); throw err;
+async function storeNewMessage(message) {
+  try {
+    return await message.save();
+  } catch (err) {
+    console.error(err);
+    throw err;
   }
 }
 
-async function getUserMessages(email){
+async function getUserMessages(email) {
+  try {
+    return await Message.find({
+      $or: [{ recipient: email }, { sender: email }],
+    }).sort({ dateSent: 1 });
+  } catch (err) {
+    console.error(err);
+    throw err;
+  }
+}
+
+async function getFriendMessages(clientEmail, friendEmail) {
   try {
     return await Message.find({
       $or: [
-        { recipient: email },
-        { sender: email }
-      ]
-    }).sort({"dateSent": 1});
-  }catch(err){
-    console.error(err); throw err;
+        { $and: [{ recipient: clientEmail }, { sender: friendEmail }] },
+        { $and: [{ recipient: friendEmail }, { sender: clientEmail }] },
+      ],
+    }).sort({ dateSent: 1 });
+  } catch (err) {
+    console.error(err);
+    throw err;
   }
 }
 
-async function getFriendMessages(clientEmail, friendEmail){
+async function setFriendMessageRead(clientEmail, friendEmail) {
   try {
-    return await Message.find({
-      $or: [
-        {$and:[
-          { recipient: clientEmail },
-          { sender: friendEmail }
-        ]},
-        {$and:[
-          { recipient: friendEmail },
-          { sender: clientEmail }
-        ]}
-      ]
-    }).sort({"dateSent": 1});
-  }catch(err){
-    console.error(err); throw err;
-  }
-}
-
-async function setFriendMessageRead(clientEmail, friendEmail){
-  try{
     return await Message.updateMany(
-      {$and:[
-          { recipient: clientEmail },
-          { sender: friendEmail }
-        ]},
-      {read: true},
-      {latest: true}
+      { $and: [{ recipient: clientEmail }, { sender: friendEmail }] },
+      { read: true },
+      { latest: true }
     );
-  }catch(err){console.error(err); throw err; }
+  } catch (err) {
+    console.error(err);
+    throw err;
+  }
 }
 
-async function getLatestMessageFromEachConversation(email){
+async function getLatestMessageFromEachConversation(email) {
   try {
     return await await Message.aggregate([
       {
         $match: {
-          $or: [
-            { recipient: email },
-            { sender: email }
-          ]
-        }
+          $or: [{ recipient: email }, { sender: email }],
+        },
       },
       {
-        $sort: { dateSent: -1 }
+        $sort: { dateSent: -1 },
       },
       {
         $group: {
@@ -94,26 +88,48 @@ async function getLatestMessageFromEachConversation(email){
               $cond: [
                 { $gte: [{ $cmp: ["$sender", "$recipient"] }, 0] },
                 { $concat: ["$recipient", "-", "$sender"] },
-                { $concat: ["$sender", "-", "$recipient"] }
-              ]
-            }
+                { $concat: ["$sender", "-", "$recipient"] },
+              ],
+            },
           },
-          latestMessage: { $first: "$$ROOT" } // Get the first (latest) message from each conversation
-        }
+          latestMessage: { $first: "$$ROOT" }, // Get the first (latest) message from each conversation
+        },
       },
       {
-        $replaceRoot: { newRoot: "$latestMessage" } // Replace the root with the latest message
-      }
-    ]).sort({"dateSent": 1});
-  }catch(err){
-    console.error(err); throw err;
+        $replaceRoot: { newRoot: "$latestMessage" }, // Replace the root with the latest message
+      },
+    ]).sort({ dateSent: 1 });
+  } catch (err) {
+    console.error(err);
+    throw err;
   }
 } //mongo aggregation used rather than in code filtering as it is more efficient
 
+async function deleteConversation(clientEmail, friendEmail) {
+  try {
+    return await Message.deleteMany({
+      $or: [
+        {
+          $and: [{ recipient: clientEmail }, { sender: friendEmail }],
+        },
+        {
+          $and: [{ recipient: friendEmail }, { sender: clientEmail }],
+        },
+      ],
+    });
+  } catch (err) {
+    console.error("Error deleting messages:", err);
+    throw err;
+  }
+}
+
 async function testOnlyDeleteAll() {
-  try{
+  try {
     return await Message.deleteMany();
-  }catch(err){console.error(err); throw err;}
+  } catch (err) {
+    console.error(err);
+    throw err;
+  }
 }
 
 module.exports = {
@@ -123,5 +139,6 @@ module.exports = {
   testOnlyDeleteAll,
   getFriendMessages,
   setFriendMessageRead,
-  getLatestMessageFromEachConversation
-}
+  deleteConversation,
+  getLatestMessageFromEachConversation,
+};
